093a607a742228aeae2ae6d9bea87ac46d3ef5be
DecwLK <carlwilland@gmail.com>
2024-01-11 16:49:10 +0100

    add lombok

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,12 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    compileOnly("org.projectlombok:lombok:1.18.30")
+    annotationProcessor("org.projectlombok:lombok:1.18.30")
+
+    testCompileOnly("org.projectlombok:lombok:1.18.30")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
 }
 
 tasks.test {

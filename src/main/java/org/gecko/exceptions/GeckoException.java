ba1d2b539680aa29c497c2ca5951ee8c6d58fc93
BenKenobie31415 <villauben@gmail.com>
2024-01-17 14:41:42 +0100

    ViewModelFactory gets available ViewModelElements from GeckoViewModel

--- src/main/java/org/gecko/exceptions/GeckoException.java
+++ src/main/java/org/gecko/exceptions/GeckoException.java
@@ -1,4 +1,7 @@
 package org.gecko.exceptions;
 
 public class GeckoException extends Exception {
+    public GeckoException(String message) {
+        super(message);
+    }
 }
---
+++ src/main/java/org/gecko/exceptions/MissingViewModelElement.java
@@ -0,0 +1,7 @@
+package org.gecko.exceptions;
+
+public class MissingViewModelElement extends GeckoException {
+    public MissingViewModelElement(String message) {
+        super(message);
+    }
+}

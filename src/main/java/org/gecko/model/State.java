880df7b6cd66d4cfa7bbd54d73ad7a080d6d2ab8
kassandraveress <150353029+kassandraveress@users.noreply.github.com>
2024-01-25 00:35:21 +0200

    Checkstyle fixes.

--- src/main/java/org/gecko/model/State.java
+++ src/main/java/org/gecko/model/State.java
@@ -1,9 +1,9 @@
 package org.gecko.model;
 
-import java.util.HashSet;
-import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.HashSet;
+import java.util.Set;
 import lombok.Getter;
 import lombok.Setter;
 

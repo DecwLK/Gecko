a44c3111dc3a0f4afb6ab33e46af585760f8ccea
kassandraveress <150353029+kassandraveress@users.noreply.github.com>
2024-01-24 14:51:11 +0200

    Implemented ProjectFileSerializer, ProjectFileParser and afferent classes and visitors.

--- src/main/java/org/gecko/model/State.java
+++ src/main/java/org/gecko/model/State.java
@@ -4,6 +4,8 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.HashSet;
 import java.util.Set;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
 

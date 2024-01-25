2c335d811203ce6150932b67acdfe1c2a2366760
kassandraveress <150353029+kassandraveress@users.noreply.github.com>
2024-01-25 15:33:32 +0200

    Handled missing properties in loaded files and integrated new project creation.

--- src/main/java/org/gecko/io/ProjectFileParser.java
+++ src/main/java/org/gecko/io/ProjectFileParser.java
@@ -1,70 +1,48 @@
 package org.gecko.io;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import javafx.util.Pair;
-import org.gecko.exceptions.MissingViewModelElement;
-import org.gecko.model.Automaton;
-import org.gecko.model.Edge;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.gecko.model.GeckoModel;
-import org.gecko.model.Region;
-import org.gecko.model.State;
 import org.gecko.model.System;
-import org.gecko.model.SystemConnection;
-import org.gecko.model.Variable;
 import org.gecko.viewmodel.GeckoViewModel;
+import org.gecko.viewmodel.PositionableViewModelElement;
 
 public class ProjectFileParser implements FileParser {
     private static final ObjectMapper objectMapper = new ObjectMapper();
+    private List<PositionableViewModelElement<?>> generatedViewModelElements;
+
+    public ProjectFileParser() {
+        generatedViewModelElements = new ArrayList<>();
+    }
 
     @Override
-    public Pair<GeckoModel, GeckoViewModel> parse(File file) throws IOException, MissingViewModelElement {
+    public Pair<GeckoModel, GeckoViewModel> parse(File file) throws IOException {
         GeckoJsonWrapper geckoJsonWrapper = objectMapper.readValue(file, GeckoJsonWrapper.class);
 
         System root = objectMapper.readValue(geckoJsonWrapper.getModel(), System.class);
         TypeReference<ArrayList<ViewModelPropertiesContainer>> typeRef = new TypeReference<>() {
         };
-        List<ViewModelPropertiesContainer> newViewModelProperties = objectMapper.readValue(geckoJsonWrapper.getViewModelProperties(), typeRef);
+        List<ViewModelPropertiesContainer> newViewModelProperties =
+            objectMapper.readValue(geckoJsonWrapper.getViewModelProperties(), typeRef);
 
         GeckoModel model = new GeckoModel(root);
         GeckoViewModel viewModel = new GeckoViewModel(model);
 
-        ViewModelElementCreatorVisitor visitor = new ViewModelElementCreatorVisitor(viewModel.getViewModelFactory(), newViewModelProperties);
-        this.visitModel(root, visitor);
+        ViewModelElementCreatorVisitor visitor =
+            new ViewModelElementCreatorVisitor(viewModel.getViewModelFactory(), newViewModelProperties,
+                generatedViewModelElements);
+        visitor.visitModel(root);
+        generatedViewModelElements = visitor.getGeneratedViewModelElements();
 
         return new Pair<>(model, viewModel);
     }
 
-    protected void visitModel(System system, ViewModelElementCreatorVisitor visitor) {
-        for (Variable variable : system.getVariables()) {
-            visitor.visit(variable);
-        }
-
-        for (SystemConnection systemConnection : system.getConnections()) {
-            visitor.visit(systemConnection);
-        }
-
-        Automaton automaton = system.getAutomaton();
-
-        for (State state : automaton.getStates()) {
-            visitor.visit(state);
-        }
-
-        for (Region region : automaton.getRegions()) {
-            visitor.visit(region);
-        }
-
-        for (Edge edge : automaton.getEdges()) {
-            visitor.visit(edge);
-        }
-
-        for (System child : system.getChildren()) {
-            visitor.visit(child);
-            visitModel(child, visitor);
-        }
+    public List<PositionableViewModelElement<?>> getGeneratedViewModelElements() {
+        return generatedViewModelElements;
     }
 }

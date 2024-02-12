6ba188b3552c2dcf377e4f3b96a56e1cadeaaca1
DecwLK <carlwilland@gmail.com>
2024-02-12 03:13:55 +0100

    fix checkstyle

--- src/main/java/org/gecko/actions/PastePositionableViewModelElementAction.java
+++ src/main/java/org/gecko/actions/PastePositionableViewModelElementAction.java
@@ -52,7 +52,8 @@ public class PastePositionableViewModelElementAction extends Action {
             if (!geckoViewModel.getCurrentEditor().getCurrentSystem().getTarget().getVariables().contains(v)) {
                 continue;
             }
-            Variable copy = geckoViewModel.getGeckoModel().getModelFactory().copyVariable(copyVisitor.getCopiedPorts().get(v));
+            Variable copy =
+                geckoViewModel.getGeckoModel().getModelFactory().copyVariable(copyVisitor.getCopiedPorts().get(v));
             geckoViewModel.getCurrentEditor().getCurrentSystem().getTarget().addVariable(copy);
             copyVisitor.getCopiedPorts().put(v, copy);
             PortViewModel portViewModel = geckoViewModel.getViewModelFactory().createPortViewModelFrom(copy);
@@ -60,7 +61,8 @@ public class PastePositionableViewModelElementAction extends Action {
             pastedElements.add(portViewModel);
         }
         for (System s : copyVisitor.getCopiedSystems().keySet()) {
-            System copy = geckoViewModel.getGeckoModel().getModelFactory().copySystem(copyVisitor.getCopiedSystems().get(s));
+            System copy =
+                geckoViewModel.getGeckoModel().getModelFactory().copySystem(copyVisitor.getCopiedSystems().get(s));
             geckoViewModel.getCurrentEditor().getCurrentSystem().getTarget().addChild(copy);
             copy.getVariables().clear();
             for (Variable v : copyVisitor.getCopiedSystems().get(s).getVariables()) {
@@ -78,7 +80,8 @@ public class PastePositionableViewModelElementAction extends Action {
 
     private boolean pasteAutomaton() throws MissingViewModelElementException {
         for (State state : copyVisitor.getCopiedStates().keySet()) {
-            State copy = geckoViewModel.getGeckoModel().getModelFactory().copyState(copyVisitor.getCopiedStates().get(state));
+            State copy =
+                geckoViewModel.getGeckoModel().getModelFactory().copyState(copyVisitor.getCopiedStates().get(state));
             geckoViewModel.getCurrentEditor().getCurrentSystem().getTarget().getAutomaton().addState(copy);
             copy.getContracts().clear();
             for (Contract c : copyVisitor.getCopiedStates().get(state).getContracts()) {
@@ -93,7 +96,8 @@ public class PastePositionableViewModelElementAction extends Action {
             pastedElements.add(stateViewModel);
         }
         for (Region region : copyVisitor.getCopiedRegions().keySet()) {
-            Region copy = geckoViewModel.getGeckoModel().getModelFactory().copyRegion(copyVisitor.getCopiedRegions().get(region));
+            Region copy =
+                geckoViewModel.getGeckoModel().getModelFactory().copyRegion(copyVisitor.getCopiedRegions().get(region));
             geckoViewModel.getCurrentEditor().getCurrentSystem().getTarget().getAutomaton().addRegion(copy);
             RegionViewModel regionViewModel = geckoViewModel.getViewModelFactory().createRegionViewModelFrom(copy);
             regionViewModel.setPosition(copyVisitor.getCopiedPosAndSize().get(region).getKey().add(pasteOffset));
@@ -101,7 +105,8 @@ public class PastePositionableViewModelElementAction extends Action {
             pastedElements.add(regionViewModel);
         }
         for (Edge edge : copyVisitor.getCopiedEdges().keySet()) {
-            Edge copy = geckoViewModel.getGeckoModel().getModelFactory().copyEdge(copyVisitor.getCopiedEdges().get(edge));
+            Edge copy =
+                geckoViewModel.getGeckoModel().getModelFactory().copyEdge(copyVisitor.getCopiedEdges().get(edge));
             geckoViewModel.getCurrentEditor().getCurrentSystem().getTarget().getAutomaton().addEdge(copy);
             copy.setSource(copyVisitor.getCopiedStates().get(edge.getSource()));
             copy.setDestination(copyVisitor.getCopiedStates().get(edge.getDestination()));

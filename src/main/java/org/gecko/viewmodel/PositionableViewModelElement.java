127de54e0057377c2d7c3560b744933d9de71653
BenKenobie31415 <villauben@gmail.com>
2024-03-10 19:09:17 +0100

    formatting

--- src/main/java/org/gecko/viewmodel/PositionableViewModelElement.java
+++ src/main/java/org/gecko/viewmodel/PositionableViewModelElement.java
@@ -10,7 +10,6 @@ import javafx.geometry.Point2D;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.Setter;
-import org.gecko.exceptions.ModelException;
 import org.gecko.model.Element;
 
 /**
